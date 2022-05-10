341ca6516da385a3e948fa8d5d33f76c120fedbb
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2022-05-10 23:52:04 +0300

    Google Map, Whatsapp Contact

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+document.addEventListener("DOMContentLoaded", init)
 const toggleMenu = document.querySelector(".toggle-menu")
 const navList = document.querySelector(".nav-list")
+
 
 toggleMenu.addEventListener("click", () => {
     toggleMenu.classList.toggle("active")
     navList.classList.toggle("active")
 })
+
+
+function init () {
+    let query = window.matchMedia("(max-width: 1375px)")
+    let query2 = window.matchMedia("(min-width: 750px)")
+
+    let whatsappContact = document.getElementById("whatsapp")
+
+    if (query.matches && query2.matches) {
+        whatsappContact.innerHTML = `<img src="whatsapp-256 (1).png" alt="WhatsApp Icon"></img>`
+    } else {
+        whatsappContact.innerHTML = `<img src="whatsapp-256 (1).png" alt="WhatsApp Icon">WhatsApp İletişim Hattı`
+    }
+}
+
+
