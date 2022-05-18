2c504c22adf40ca64b439e174dbfa23d05631e11
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2022-05-18 13:22:43 +0300

    pop-up

--- index.js
+++ index.js
@@ -17,13 +17,46 @@ function init () {
     let whatsappContact = document.getElementById("whatsapp")
 
     if (query.matches && query2.matches) {
-        whatsappContact.innerHTML = `<img src="whatsapp-256 (1).png" alt="WhatsApp Icon"></img>`
+        whatsappContact.innerHTML = `<img src="images/wapp-icon.png" alt="WhatsApp Icon"></img>`
     } else {
-        whatsappContact.innerHTML = `<img src="whatsapp-256 (1).png" alt="WhatsApp Icon">WhatsApp İletişim Hattı`
+        whatsappContact.innerHTML = `<img src="images/wapp-icon.png" alt="WhatsApp Icon">WhatsApp İletişim Hattı`
     }
 }
 
 // Pop-up
+const modal1 = document.getElementById("more-contact")
+const modal2 = document.getElementById("more-sale")
+const modal3 = document.getElementById("more-faq")
+
 const moreButton1 = document.querySelector(".open-button-st")
 const moreButton2 = document.querySelector(".open-button-nd")
 const moreButton3 = document.querySelector(".open-button-rd")
+
+const closeButton1 = document.querySelector(".close-button-st")
+const closeButton2 = document.querySelector(".close-button-nd")
+const closeButton3 = document.querySelector(".close-button-rd")
+
+
+
+moreButton1.addEventListener("click", () => {
+    modal1.showModal();
+})
+closeButton1.addEventListener("click", () => {
+    modal1.close();
+})
+
+
+moreButton2.addEventListener("click", () => {
+    modal2.showModal();
+})
+closeButton2.addEventListener("click", () => {
+    modal2.close();
+})
+
+
+moreButton3.addEventListener("click", () => {
+    modal3.showModal();
+})
+closeButton3.addEventListener("click", () => {
+    modal3.close();
+})
