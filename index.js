6457cc83c154ef8f7864cf3061f03205695b36ec
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2022-05-22 12:50:43 +0300

    content

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-// Responsive 
-document.addEventListener("DOMContentLoaded", init)
-const toggleMenu = document.querySelector(".toggle-menu")
-const navList = document.querySelector(".nav-list")
-
-
-toggleMenu.addEventListener("click", () => {
-    toggleMenu.classList.toggle("active")
-    navList.classList.toggle("active")
-})
-
-
-function init () {
-    let query = window.matchMedia("(max-width: 1375px)")
-    let query2 = window.matchMedia("(min-width: 750px)")
-
-    let whatsappContact = document.getElementById("whatsapp")
-
-    if (query.matches && query2.matches) {
-        whatsappContact.innerHTML = `<img src="../images/wapp-icon.png" alt="Klima alım-satışçısı ile iletişim kurmak için Whatsapp görüşme linki"></img>`
-    } else {
-        whatsappContact.innerHTML = `<img src="../images/wapp-icon.png" alt="Klima alım-satışçısı ile iletişim kurmak için Whatsapp görüşme linki">WhatsApp İletişim Hattı`
-    }
-}
-
 // Pop-up
 const modal1 = document.getElementById("more-contact")
 const modal2 = document.getElementById("more-sale")
